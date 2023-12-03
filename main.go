5d1a35c91edf536bbe3affe0a36fa92a88f15dc5
Alextopher <christopher.mahoney2000@gmail.com>
2023-12-02 19:23:05 -0500

    `main:` sync on the 15 minute mark

--- main.go
+++ main.go
@@ -11,15 +11,13 @@ import (
 func main() {
 	file, err := os.Open("config.json")
 	if err != nil {
-		log.Println("Error opening config file: ", err)
-		return
+		log.Fatalln("Error opening config file: ", err)
 	}
 
 	// Parse the config file
 	config, err := ParseConfig(file)
 	if err != nil {
-		log.Println("Error parsing config file: ", err)
-		return
+		log.Fatalln("Error parsing config file: ", err)
 	}
 
 	file.Close()
@@ -27,8 +25,7 @@ func main() {
 	// Create a new Discord session using the provided bot token.
 	session, err := discordgo.New("Bot " + config.DiscordToken)
 	if err != nil {
-		log.Println("Error creating Discord session: ", err)
-		return
+		log.Fatalln("Error creating Discord session: ", err)
 	}
 
 	// Create a new bot
@@ -37,24 +34,21 @@ func main() {
 	// Start the bot
 	err = bot.Start()
 	if err != nil {
-		log.Println("Error starting bot: ", err)
-		return
+		log.Fatalln("Error starting bot: ", err)
 	}
 
 	// Add guilds
 	for guildID, guildConfig := range config.Guilds {
 		err = bot.AddGuild(guildID, guildConfig.Year, guildConfig.LeaderboardID)
 		if err != nil {
-			log.Println("Error adding guild: ", err)
-			return
+			log.Fatalln("Error adding guild: ", err)
 		}
 	}
 
 	// Register commands
 	err = bot.RegisterCommands()
 	if err != nil {
-		log.Println("Error registering commands: ", err)
-		return
+		log.Fatalln("Error registering commands: ", err)
 	}
 
 	// Register handlers
@@ -64,8 +58,7 @@ func main() {
 	for _, guild := range bot.session.State.Guilds {
 		err = bot.CreateRoles(guild)
 		if err != nil {
-			log.Println("Error creating roles: ", err)
-			return
+			log.Fatalln("Error creating roles: ", err)
 		}
 	}
 
@@ -74,7 +67,16 @@ func main() {
 	// Every 15 minutes, sync the bot with the Advent of Code API
 	bot.Sync()
 
+	// Sleep until the next 15 minute mark
+	now := time.Now()
+	minutes := time.Duration(15 - (now.Minute() % 15))
+	seconds := time.Duration(60 - now.Second())
+	time.Sleep(minutes*time.Minute + seconds*time.Second)
+
 	ticker := time.NewTicker(15 * time.Minute)
+
+	// Start syncing every 15 minutes
+	bot.Sync()
 	for {
 		<-ticker.C
 		bot.Sync()
